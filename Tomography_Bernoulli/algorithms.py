7797fe2394890f76fa2b5d235434c47bcd9096c1
tem373 <tem373@nyu.edu>
2018-02-13 13:46:16 -0500

    update

---
+++ Tomography_Bernoulli/algorithms.py
@@ -0,0 +1,22 @@
+
+
+def est_bernoulli_prob(k):
+    """Main function for estimating the bernoulli packet drop probability
+    in each link. Takes as input link k and calculates the alpha"""
+
+    find_gamma(k)
+    infer(k, 1)
+
+
+def find_gamma(k):
+
+
+
+
+
+def infer(k, A):
+
+
+
+
+

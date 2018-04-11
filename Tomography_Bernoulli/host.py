3b5b8fcde77b7d09cdf2cb15f383bc8351f07369
tem373 <tem373@nyu.edu>
2018-04-11 11:56:24 -0400

    implemented Dr. Sivaraman's SDN reporting method, misc cleanup of the other files

--- Tomography_Bernoulli/host.py
+++ Tomography_Bernoulli/host.py
@@ -6,9 +6,6 @@ class Host:
         self.in_order_rx_seq = -1       # Sequence number accounting
         self.name = name        
         self.downstream_nodes = []
-        
-
-        # To keep track of 
         self.success_queue = []
 
         # To check if it is a receiver
@@ -28,38 +25,24 @@ class Host:
             
             # Create new packet
             new_seq_num = self.in_order_rx_seq + 1
-            #new_packet = Packet(tick)            
-            #new_packet = Packet(tick, new_seq_num)
             
             # Deliver packet to the link (or links if multicast)
             for link in links:
                 new_packet = Packet(tick)
 
                 # Check if packet got dropped
-                #if(self.success_queue != [] and self.success_queue[tick] == 0):
                 if(self.success_queue[tick] == 0):                
                     new_packet.was_dropped = True
-                #print("Drop Status: " + str(new_packet.was_dropped))
 
                 link.recv(new_packet)
-                #print("Host " + self.name + " sent packet #: " + str(new_packet.seq_num) + " status: " + str(new_packet.was_dropped))
 
-            # possibly have to add a packet sent time
             self.ready_to_send = False
-            #print("Host " + self.name + " sent packet #: " + str(new_packet.seq_num))
 
 
     def recv(self, pkt, tick):
-        # Update sequence numbers        
-        #if (pkt.seq_num - self.in_order_rx_seq == 1):
         if (pkt.was_dropped == False):        
             self.in_order_rx_seq = pkt.seq_num
             self.ready_to_send = True
-
-            # Accounting for dropped packet
-            # Only leaf nodes can perform measurement (we here assume routers have
-            # no measurement functionality. Perform this test to see if a node
-            # is an endpoint and if so measure successes/failures
 
             self.success_queue.append(1)
 
@@ -70,15 +53,5 @@ class Host:
             
             # Accounting for dropped packets
             self.success_queue.append(0)
-
-        #print("Host " + self.name + " success queue: " + str(self.success_queue))
-
-
-
-
-
-
-
-
 
 
--- Tomography_Bernoulli/network.py
+++ Tomography_Bernoulli/network.py
@@ -5,23 +5,31 @@ class Link:
     def __init__(self, bernoulli_alpha):
         self.link_queue = []
         self.bernoulli_alpha = bernoulli_alpha
-        #self.name = name
+        self.total_received_pkts = 0
+        self.total_dropped_pkts = 0
 
-    # Not sure if enforcing queue limit is needed here
+
+    def loss_rate(self):
+        if (self.total_received_pkts == 0):
+            return -1
+        else:
+            return (self.total_dropped_pkts * 1.0) / self.total_received_pkts    
+
+
     def recv(self, pkt):
         
         if(pkt.was_dropped == True):
             self.link_queue.append(pkt)
         elif(pkt.was_dropped == False):
+            self.total_received_pkts +=1
             droptest = self.isdropped()
             if (droptest == False):
                 self.link_queue.append(pkt)
             elif (droptest == True):
+                self.total_dropped_pkts += 1
                 pkt.was_dropped = True
                 self.link_queue.append(pkt)
-        #else:
-        #    self.link_queue.append(pkt)
-        #print("Drop status: " + str(pkt.was_dropped))
+
 
     def tick(self, tick, host):
         if(len(self.link_queue) != 0):
@@ -29,6 +37,7 @@ class Link:
             # "Shift" queue upward one
             self.link_queue = self.link_queue[1:]
             host.recv(head, tick)
+
 
     def isdropped(self):
         rand_number = random.uniform(0,1)

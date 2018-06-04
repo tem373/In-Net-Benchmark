3e30072741d7531700ad44f29f5c976a42f166ff
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-06-04 16:12:01 -0400

    WIP commit to sanity check MLE

--- tomography.py
+++ tomography.py
@@ -52,16 +52,38 @@ class TomographyMle(object):
     if (tree.left == None and tree.right == None):
       tree.A = tree.gamma # Treat this as though the product is 0 per the paper
     else:
-      if (tree.left.gamma + tree.right.gamma - tree.gamma == 0):
-        tree.A = -1       # Need to handle divide by zero
-      else:               # closed form solution for binary trees
-        tree.A = (tree.left.gamma * tree.right.gamma * 1.0) / (tree.left.gamma + tree.right.gamma - tree.gamma)
-    # assert(tree.A > 0) # This may fail for the first several probes
-    # assert(tree.A < 1) # This may fail for the first several probes
+      assert (tree.left.gamma + tree.right.gamma - tree.gamma != 0)
+      # closed form solution for binary trees
+      tree.A = (tree.left.gamma * tree.right.gamma * 1.0) / (tree.left.gamma + tree.right.gamma - tree.gamma)
     tree.alpha = tree.A * 1.0 / total_A
     if (tree.left != None and tree.right != None):
       TomographyMle.compute_mle(tree.left,  tree.A)
       TomographyMle.compute_mle(tree.right, tree.A)
+
+  @staticmethod
+  # Check conditions i and iv from 5.1 of http://nickduffield.net/download/papers/minctoit.pdf
+  def pre_sanity_check(root):
+    for node in root.nodes():
+      if node != root:
+        if node.gamma == 0: # condition i
+          print("Condition i: node.gamma is", node.gamma)
+          return False
+        elif ((node.left != None) and (node.right != None)): # condition iv
+          if (node.left.gamma + node.right.gamma - node.gamma == 0):
+            print("Condition iv: node.left.gamma, node.right.gamma, node.gamma", node.left.gamma, node.right.gamma, node.gamma)
+            return False
+    return True
+
+  @staticmethod
+  # Check conditions ii and iii from 5.1 of http://nickduffield.net/download/papers/minctoit.pdf
+  def post_sanity_check(root):
+    for node in root.nodes():
+      if node != root:
+        if (node.alpha <= 0) or (node.alpha >= 1): # condition i
+          print("Condition ii/iii: node.alpha is", node.alpha)
+          return False
+    return True
+
 
 if len(sys.argv) != 6:
   print("Usage: ", sys.argv[0], " depth loss_probability loss_type num_probes num_trials ")
@@ -82,36 +104,6 @@ for i in range(1, num_trials + 1):
   # seed random number generator
   numpy.random.seed(i)
 
-  # multicast tomography based approach
-  mcast_tree = Tree(depth, loss_probability, loss_type)
-  probe = dict() # To store results of probes keyed by receiver ID
-  for receiver in mcast_tree.receivers():
-    probe[receiver.id] = 0
-  TomographyMle.create_estimator(mcast_tree)
-  for i in range(0, num_probes):
-    if loss_type == "gilbert_elliot":
-      for node in mcast_tree.nodes():
-        node.state_transition()
-    outcome = mcast_tree.send_multicast_probe(i) # Pass in probe number i as tick to send_multicast_probe
-    for rx_tuple in outcome:
-      probe[rx_tuple[0]] = 1 if rx_tuple[1] else 0
-    TomographyMle.update_Y(mcast_tree, probe)
-
-  # Now compute MLE
-  TomographyMle.compute_gamma(mcast_tree)
-  TomographyMle.compute_mle(mcast_tree, 1.0)
-
-  # Compute max errors for tomography
-  node_tomography_errors = []
-  for node in mcast_tree.nodes():
-    if node != mcast_tree:
-      node_tomography_errors += [round(100.0 * abs(1 - node.alpha - float(loss_probability)) / float(loss_probability), 5)]
-
-  if (mean(node_tomography_errors) > 1e10):
-    print("Abnormally high tomography error. Omitting this trial from error calculation.")
-  else:
-    mean_tomography_errors += [mean(node_tomography_errors)]
-
   # in network approach
   in_network_tree = Tree(depth, loss_probability, loss_type)
   for i in range(0, num_probes):
@@ -130,6 +122,42 @@ for i in range(1, num_trials + 1):
     print("Abnormally high in-network error. Omitting this trial from error calculation.")
   else:
     mean_true_errors += [mean(node_true_errors)]
+
+  # multicast tomography based approach
+  mcast_tree = Tree(depth, loss_probability, loss_type)
+  probe = dict() # To store results of probes keyed by receiver ID
+  for receiver in mcast_tree.receivers():
+    probe[receiver.id] = 0
+  TomographyMle.create_estimator(mcast_tree)
+  for i in range(0, num_probes):
+    if loss_type == "gilbert_elliot":
+      for node in mcast_tree.nodes():
+        node.state_transition()
+    outcome = mcast_tree.send_multicast_probe(i) # Pass in probe number i as tick to send_multicast_probe
+    for rx_tuple in outcome:
+      probe[rx_tuple[0]] = 1 if rx_tuple[1] else 0
+    TomographyMle.update_Y(mcast_tree, probe)
+
+  # Now compute MLE
+  TomographyMle.compute_gamma(mcast_tree)
+  if (TomographyMle.pre_sanity_check(mcast_tree) == False):
+    print("Pre sanity check failed. Skipping this trial.\n")
+    continue
+  TomographyMle.compute_mle(mcast_tree, 1.0)
+  if (TomographyMle.post_sanity_check(mcast_tree) == False):
+    print("Post sanity check failed. Skipping this trial.\n")
+    continue
+
+  # Compute max errors for tomography
+  node_tomography_errors = []
+  for node in mcast_tree.nodes():
+    if node != mcast_tree:
+      node_tomography_errors += [round(100.0 * abs(1 - node.alpha - float(loss_probability)) / float(loss_probability), 5)]
+
+  if (mean(node_tomography_errors) > 1e10):
+    print("Abnormally high tomography error. Omitting this trial from error calculation.")
+  else:
+    mean_tomography_errors += [mean(node_tomography_errors)]
 
 # print out average of max errors
 print("Depth =", depth, "loss_probability =", loss_probability, "loss_type =", loss_type, \
